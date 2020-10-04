c75a69a72b1b48f26715b85e219a9e3e2309cd5a
AmitNirGit <67857460+AmitNirGit@users.noreply.github.com>
2020-10-04 16:09:11 +0300

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -77,39 +77,9 @@ describe(`${projectName} - test suite`, () => {
     expect(resultsValue).toBe('10');
   });
 
-  it('Has working parentheses', async () => {
-    await page.goto('http://localhost:3000/', { waitUntil: 'networkidle0' });
-    await page.click('#op_leftParentheses');
-    await page.click('#digit_1');
-    await page.click(`#op_plus`);
-    await page.click('#digit_9');
-    await page.click('#op_rightParentheses');
-    await page.click(`#op_multi`);
-    await page.click('#digit_2');
-    await page.click('#equal');
-    const result = await page.$('.result');
-    const resultsValue = await (
-      await result.getProperty('innerText')
-    ).jsonValue();
-    expect(resultsValue).toBe('20');
-  });
 
-  it('Order of operations', async () => {
-    await page.goto('http://localhost:3000/', { waitUntil: 'networkidle0' });
-    await page.click('#digit_1');
-    await page.click(`#op_plus`);
-    await page.click('#digit_9');
-    await page.click(`#op_multi`);
-    await page.click('#digit_2');
-    await page.click('#equal');
-    const result = await page.$('.result');
-    const resultsValue = await (
-      await result.getProperty('innerText')
-    ).jsonValue();
-    expect(resultsValue).toBe('19');
-  });
 
-  it(`Changes the input to an 'error' string if user tries to devide by 0`, async () => {
+  it(`Changes the input to an 'Error' string if user tries to devide by 0`, async () => {
     await page.goto('http://localhost:3000/', { waitUntil: 'networkidle0' });
     await page.click('#digit_5');
     await page.click('#op_divide');
@@ -119,6 +89,6 @@ describe(`${projectName} - test suite`, () => {
     const resultsValue = await (
       await result.getProperty('innerText')
     ).jsonValue();
-    expect(resultsValue).toBe('error');
+    expect(resultsValue).toBe('Error');
   });
 });
